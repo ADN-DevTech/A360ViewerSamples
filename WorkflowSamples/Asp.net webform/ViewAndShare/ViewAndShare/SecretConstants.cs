5e94d3cd9cdc9b2056e919b67d4617a65f754edf
Daniel Du <duchangyu@gmail.com>
2014-07-02 17:47:14 +0800

    drag drop view and share

--- WorkflowSamples/Asp.net webform/ViewAndShare/ViewAndShare/SecretConstants.cs
+++ WorkflowSamples/Asp.net webform/ViewAndShare/ViewAndShare/SecretConstants.cs
@@ -7,12 +7,16 @@ namespace ViewAndShare
 {
     public class SecretConstants
     {
+        //public static string BASE_URL = "https://developer-stg.api.autodesk.com";
+        //public static string DEFAULT_BUCKET_KEY = "daniel_translate_bucket";
 
+        //public static string CLIENT_ID = "MdlCX6Vpt5xs2IUN3A98aaCSCPst7cQw";
+        //public static string CLIENT_SECRET = "UIwCY4rxoZXrvpZj";
 
         public static string BASE_URL = "https://developer.api.autodesk.com";
         public static string DEFAULT_BUCKET_KEY = "daniel_translate_bucket";
 
-        public static string CLIENT_ID = "your client id";
-        public static string CLIENT_SECRET = "your client secret";
+        public static string CLIENT_ID = "gP4GrUabh8o5RdTNwuGmHFFeNjt7iw3p";
+        public static string CLIENT_SECRET = "yNtTGopBIGAqaWYG";
     }
 }
